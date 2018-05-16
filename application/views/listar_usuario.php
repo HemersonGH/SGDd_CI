59513d429feb5165998c39fd378949120fcb6323
Hemerson-GH <hemersonel@gmail.com>
2018-05-16 01:43:32 -0300

    construindo tela usuario

---
+++ application/views/listar_usuario.php
@@ -0,0 +1,12 @@
+
+  <main class="col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3 main">
+    <div class="col-md-12">
+      <h1>Usuários</h1>
+    </div>
+
+    <div class="col-md-2 position">
+      <a class="btn btn-primary btn-block" href="<?= base_url();?>usuario/cadastro"> Novo Usuário</a>
+    </div>
+  </main>
+  </div>
+  </div>
